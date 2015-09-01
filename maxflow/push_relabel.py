f19588973fcb77a22c04de9af8779619619699f0
Jovan Cejovic <jovan.cejovic@sbgenomics.com>
2015-09-01 13:40:38 +0200

    Add push-relabel stub

---
+++ maxflow/push_relabel.py
@@ -0,0 +1,20 @@
+__author__ = 'Jovan Cejovic <jovan.cejovic@sbgenomics.com>'
+__date__ = '30 August 2015'
+__copyright__ = 'Copyright (c) 2015 Seven Bridges Genomics'
+
+
+def push(flow_network):
+    pass
+
+
+def relabel(flow_network):
+    pass
+
+
+def init_preflow(flow_network):
+    pass
+
+
+def generic_push_relabel(flow_network):
+    excess = {}
+    height = {}
