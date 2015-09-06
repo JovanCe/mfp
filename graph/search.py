f57393aeee00f7fc024fda50ab5ffe4da7319fa5
Jovan Cejovic <jovan.cejovic@sbgenomics.com>
2015-09-06 16:51:38 +0200

    Add another bfs implementation which finds a path with every edge containing greater capacity than specified

--- graph/search.py
+++ graph/search.py
@@ -45,6 +45,45 @@ def bfs(graph, source, target):
     return []
 
 
+def bfs_min_capacity(flow_network, source, target, min_capacity):
+    """
+    Finds a path from source to target using breadth-first search
+    :param graph:
+    :param source:
+    :param target:
+    :return:
+    """
+    nodes = flow_network.get_nodes()
+    queue = Queue(len(nodes))
+    distances = {}
+    parents = {}
+    for n in nodes:
+        # infinite distance
+        distances[n] = -1
+        # no parent
+        parents[n] = -1
+
+    distances[source] = 0
+    queue.put(source)
+    while not queue.empty():
+        u = queue.get()
+        if u == target:
+            path = []
+            while parents[u] != -1:
+                path.insert(0, u)
+                u = parents[u]
+            # check if a path exists and add the source node for complete path
+            if len(path):
+                path.insert(0, u)
+            return path
+        for n in flow_network.get_node_neighbours(u):
+            if distances[n] == -1 and flow_network.get_arc_capacity(u, n) >= min_capacity:
+                distances[n] = distances[u] + 1
+                parents[n] = u
+                queue.put(n)
+
+    return []
+
 def dfs(graph, node):
     pass
 
