c9e6e750f56212575b2e14b51af40879cc2b5435
Jovan Cejovic <jovan.cejovic@sbgenomics.com>
2015-09-08 23:50:04 +0200

    Move util module to top level

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ __copyright__ = 'Copyright (c) 2015 Seven Bridges Genomics'
 
 import os
 
-DATA_DIR = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')), 'data')
+DATA_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
 
 
 def get_data_file(filename):
