6669d42cc972784c44ccdc85e65e37ef207d9f5e
Jovan <jovan.cejovic@gmail.com>
2015-09-14 00:42:51 +0200

    Tweak util functions

--- util.py
+++ util.py
@@ -5,7 +5,12 @@ __copyright__ = 'Copyright (c) 2015 Seven Bridges Genomics'
 import os
 
 DATA_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
+OUT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'out')
 
 
 def get_data_file(filename):
     return os.path.join(DATA_DIR, filename)
+
+
+def get_out_file(*paths):
+    return os.path.join(OUT_DIR, *paths)
